047dac2a0f09ce2b182a52f7b3fc152ea493f26e
Ritesh Maurya <riteshkumar411552@gmail.com>
2025-12-28 16:12:37 +0530

    day3

---
+++ terraform/Day-03/main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-south-1"
+  
+}
+
+module "ec2_instance" {
+    source = "./modules/ec2_instance"
+    ami_value="ami-02b8269d5e85954ef" #replace this
+    instance_type_value="t3.micro"
+  
+}
